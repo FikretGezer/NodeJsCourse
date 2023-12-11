e6186f0dda3044690df99ebae9de9236db17fbcc
Mahmut Fikret Gezer <64322071+FikretGezer@users.noreply.github.com>
2023-12-11 07:00:56 +0300

    Params and Queries

--- Express/4-params.js
+++ Express/4-params.js
@@ -33,27 +33,6 @@ app.get('/api/products/:productID', (req, res) => {
     res.json(prod);
 });
 
-app.get('/api/v1/query', (req, res) => {
-    // console.log(req.query);
-    const {search, limit} = req.query;
-    let sortedProducts = [...products];
-
-    if(search)
-    {
-        sortedProducts = sortedProducts.filter(product => product.name.startsWith(search));
-    }
-    if(limit)
-    {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if(sortedProducts.length < 1)
-    {
-        // res.status(200).send('No products matched your search');
-        return res.status(200).json({success: true, data: []});
-    }
-    res.status(200).json(sortedProducts);
-})
-
 app.listen(5000, () => {
     console.log(`Server is listening at port 5000`);
 });
